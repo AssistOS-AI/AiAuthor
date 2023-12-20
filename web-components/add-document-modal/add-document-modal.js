49e551f23df85007b887a459cf9d06e825cf0229
Koroem <conduratvictor@yahoo.com>
2023-12-20 11:10:25 +0200

    add document modal, docId extraction from flow

--- web-components/add-document-modal/add-document-modal.js
+++ web-components/add-document-modal/add-document-modal.js
@@ -14,7 +14,8 @@ export class addDocumentModal {
         let formData = await webSkel.UtilsService.extractFormInformation(_target);
         if(formData.isValid) {
             let flowId = webSkel.currentUser.space.getFlowIdByName("AddDocument");
-            let docId = await webSkel.getService("LlmsService").callFlow(flowId, formData.data.documentTitle, formData.data.documentTopic).responseString;
+            debugger;
+            let docId = await webSkel.getService("LlmsService").callFlow(flowId, formData.data.documentTitle, formData.data.documentTopic);
             docId.responseString? docId = docId.responseString : docId = docId.responseJson;
             webSkel.UtilsService.closeModal(_target);
             await webSkel.changeToDynamicPage(`document-view-page`, `documents/${docId}/document-view-page`);
