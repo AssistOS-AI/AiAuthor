f1d08e8b5e16171832fe29423d56b5cb8d08e82e
Koroem <conduratvictor@yahoo.com>
2024-01-04 12:16:44 +0200

    added routing Service

---
+++ services/routingService.js
@@ -0,0 +1,16 @@
+export class routingService{
+    constructor(){}
+    async navigateToApplication(location){
+        const locationFragments=location.split('/');
+        let documentIdURL = locationFragments[0];
+        let presenterName = locationFragments[1];
+        let chapterIdURL = locationFragments[2];
+        let paragraphIdURL = locationFragments[3];
+        if (await storageManager.loadObject(webSkel.currentUser.space.id, "documents", documentIdURL) !== null) {
+            webSkel.currentUser.space.currentDocumentId = documentIdURL;
+            webSkel.currentUser.space.currentChapterId = chapterIdURL;
+            webSkel.currentUser.space.currentParagraphId = paragraphIdURL;
+        }
+        await webSkel.changeToDynamicPage(presenterName, url.slice(1));
+    }
+}
