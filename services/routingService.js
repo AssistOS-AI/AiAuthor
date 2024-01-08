c8578a130248f1da0a02673d2b3e1ef830aed23a
Koroem <conduratvictor@yahoo.com>
2024-01-08 13:23:46 +0200

    Updates

--- services/routingService.js
+++ services/routingService.js
@@ -1,22 +1,18 @@
 export class routingService {
-    constructor() {
-        this.appName = "AiAuthor";
-    }
-    async navigateToLocation(locationArray) {
+    constructor() {}
+    async navigateToLocation(locationArray,appName) {
         if (locationArray[0] !== "documents" && locationArray[0] !== "documents-page") {
-            console.error("Invalid URL structure.");
+            console.error("Invalid URL");
             return;
         }
-
-        if (locationArray[0] === "documents-page") {
+        if (locationArray[0] === "documents-page" || locationArray === null) {
             const pageComponent = "documents-page";
-            const pageUrl = `${webSkel.currentUser.space.id}/${this.appName}/documents-page`;
+            const pageUrl = `${webSkel.currentUser.space.id}/${appName}/documents-page`;
             await webSkel.changeToDynamicPage(pageComponent, pageUrl);
             return;
         }
-
         const webComponentName = locationArray[locationArray.length - 1];
-        const pageUrl = `${webSkel.currentUser.space.id}/${this.appName}/${locationArray.join("/")}`;
+        const pageUrl = `${webSkel.currentUser.space.id}/${appName}/${locationArray.join("/")}`;
         await webSkel.changeToDynamicPage(webComponentName, pageUrl);
     }
 }
