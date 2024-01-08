fed2f8fa0ea99ff33a8570dd6ce56a3ad9e4e156
Koroem <conduratvictor@yahoo.com>
2024-01-08 14:02:44 +0200

    Updates

--- services/routingService.js
+++ services/routingService.js
@@ -1,16 +1,22 @@
 export class routingService {
     constructor() {}
     async navigateToLocation(locationArray,appName) {
-        if (locationArray[0] !== "documents" && locationArray[0] !== "documents-page") {
-            console.error("Invalid URL");
+        if(locationArray === undefined){
+            const pageUrl = `${webSkel.currentUser.space.id}/${appName}/documents-page`;
+            await webSkel.changeToDynamicPage(pageComponent, pageUrl);
             return;
         }
-        if (locationArray[0] === "documents-page" || locationArray === null) {
+        if (locationArray[0] === "documents-page") {
             const pageComponent = "documents-page";
             const pageUrl = `${webSkel.currentUser.space.id}/${appName}/documents-page`;
             await webSkel.changeToDynamicPage(pageComponent, pageUrl);
             return;
         }
+        if (locationArray[0] !== "documents" && locationArray[0] !== "documents-page") {
+            console.error("Invalid URL");
+            return;
+        }
+
         const webComponentName = locationArray[locationArray.length - 1];
         const pageUrl = `${webSkel.currentUser.space.id}/${appName}/${locationArray.join("/")}`;
         await webSkel.changeToDynamicPage(webComponentName, pageUrl);
