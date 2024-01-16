47704271fa340a2bfd89c3b56fc7ce5d899887de
Koroem <conduratvictor@yahoo.com>
2024-01-16 10:54:07 +0200

    Breadcrumbs update

--- web-components/document-view-page/document-view-page.js
+++ web-components/document-view-page/document-view-page.js
@@ -437,7 +437,7 @@ export class documentViewPage {
         await webSkel.changeToDynamicPage("document-view-page", `${getBasePath()}/documents/${this._document.id}/document-view-page`);
     }
     async openDocumentsPage() {
-        await webSkel.changeToDynamicPage("aiauthor-documents-page", `${getBasePath()}/aiauthor-documents-page`);
+        await webSkel.changeToDynamicPage("documents-page", `${getBasePath()}/documents-page`);
     }
     async openChapterEditor() {
         await webSkel.changeToDynamicPage("chapter-editor-page", `${getBasePath()}/documents/${this._document.id}/chapters/${webSkel.currentUser.space.currentChapterId}/chapter-editor-page`);
